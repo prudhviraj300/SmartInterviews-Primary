d65593f8108d8562b4e9a37fb8a8bd5870723271
Prudhvi Raj Baki <125185503+prudhviraj300@users.noreply.github.com>
2024-09-08 15:00:58 +0530

    Create Reverse Bits .cpp

---
+++ Reverse Bits .cpp
@@ -0,0 +1,105 @@
+// Given a number, reverse the bits in the binary representation (consider 32-bit unsigned data) of the number, and print the new number formed.
+
+
+
+// Input Format
+
+// The first line of input contains T - the number of test cases. Each of the next T lines contains a number integer N.
+
+
+
+// Output Format
+
+// For each test case, print the new number formed after reversing the bits, separated by a new line.
+
+
+
+// Constraints
+
+// 1 <= T <= 100000
+
+// 0 <= N <= 109
+
+
+
+// Example
+
+// Input
+
+// 4
+
+// 4
+
+// 15
+
+// 6
+
+// 1000000000
+
+
+
+// Output
+
+// 536870912
+
+// 4026531840
+
+// 1610612736
+
+// 5462492
+
+
+
+// Explanation
+
+
+
+// Test-Case 1
+
+// Binary Representation of 4: 000...100
+
+// After reversing the bits: 001...000 (536870912)
+
+
+
+// Test-Case 2
+
+// Binary Representation of 15: 000...1111
+
+// After reversing the bits: 1111...000 (4026531840)
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+long long bin(long long n){
+    int a[32]={0};
+    int i=31;
+    if(n==0){
+        a[i--]=0;
+    }
+    while(n){
+        a[i--]=n%2;
+        n /= 2;
+    }
+    long long sum = 0;
+    for(int j=31;j>=0;j--){
+        if(a[j]==1){
+            sum += pow(2,j);
+        }
+    }
+    return sum;
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int t;
+    cin >> t;
+    while(t--){
+        long long n,ans;
+        cin >> n;
+        ans = bin(n);
+        cout << ans<< endl;
+    }  
+    return 0;
+}
